1a7fd109cc1f6eda6be85c8ae89377820f348574
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2025-01-31 14:50:37 -0300

    Recuperar senha e lista de convidados

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect
 from convidados import (
     Convidado,
     convidados,
@@ -28,6 +28,13 @@ def verificar():
 
     return render_template(
         "index.html", convidado=nome, title="Verificar convidado na lista"
+    )
+
+
+@app.get("/lista")
+def lista():
+    return render_template(
+        "lista.html", convidados=convidados, title="Lista de convidados"
     )
 
 
@@ -77,6 +84,30 @@ def cadastrar():
     return render_template(
         "convidado.html", convidado=novo_convidado, title="Convidado"
     )
+
+
+@app.get("/recuperar_senha")
+def recuperar_senha_page():
+    return render_template("recuperar_senha.html", title="Recuperar senha")
+
+
+@app.post("/recuperar_senha")
+def recuperar_senha():
+    email = request.form["email"]
+    nome = request.form["nome"]
+    senha = request.form["senha"]
+
+    convidado = procurar_convidado_pelo_email(email)
+
+    if convidado is None or convidado.nome.lower() != nome.lower():
+        flash("O e-mail não existe ou o nome do convidado não está correto", "error")
+        return render_template(
+            "recuperar_senha.html", email=email, nome=nome, title="Recuperar senha"
+        )
+
+    convidado.senha = senha
+    flash("Senha alterada com sucesso!", "info")
+    return redirect("/login")
 
 
 app.run(debug=True)
